0908f6e7df8b7dbdb54c9de0ed72c90d123dcc52
NoelThoujanba <84390146+NoelThoujanba@users.noreply.github.com>
2024-09-05 09:42:02 +0530

    Update main.py
    Added variable ffmpeg paths for mutli os support
    Added playlist support
    Added selective play for playlist support

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from discord.ext import commands
 
 import downloader as dwn
 
-import random
-
 import glob
+import os
 
 from sys import argv
+
+IsPlaying = False
 
 MUSIC_DIR = "music/"
 
@@ -16,19 +17,28 @@ command_prefix='.'
 intents = discord.Intents().all()
 intents.message_content = True
 intents.voice_states = True
-#client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix=command_prefix,intents=intents)
 
+
+def GetFFMPEGPath():
+    with open("ffmpegpath", "r") as pathfile:
+        path = pathfile.read()
+    return path
+
+FFMPEGPath = GetFFMPEGPath()
+
 @bot.command(
-    name='play',
-    description='Plays music, duh....',
+    name='Play',
     pass_context=True,
 )
 async def play(context, path):
+    if not os.path.isfile(MUSIC_DIR+path):
+        await context.send("No music file found!")
+        return
+
     # grab the user who sent the command
     user=context.message.author
     voice_channel=user.voice.channel
-    channel=None
     # only play music if user is in a voice channel
     if voice_channel!= None:
         # grab user's voice channel
@@ -36,39 +46,126 @@ async def play(context, path):
         await context.send('User is in channel: '+ channel)
         # create StreamPlayer
         vc= await voice_channel.connect()
-        vc.play(discord.FFmpegPCMAudio(MUSIC_DIR+path, executable="ffmpeg/ffmpeg"), after=lambda e: vc.stop())
+        await context.send(f"Playing {MUSIC_DIR+path}")
+        vc.play(discord.FFmpegPCMAudio(MUSIC_DIR+path, executable=FFMPEGPath), after=lambda e: vc.stop())
         # disconnect after the player has finished
         #TODO: 
         
     else:
         await context.send('User is not in a channel.')
 
-@bot.command(name="playlist", pass_context=True)
-async def play_playlist(context, dir):    
-    # grab the user who sent the command
-    user=context.message.author
-    voice_channel=user.voice.channel
-    channel=None
-    # only play music if user is in a voice channel
-    if voice_channel!= None:
-        # grab user's voice channel
-        channel=voice_channel.name
-        await context.send('User is in channel: '+ channel)
-        # create StreamPlayer
-        vc= await voice_channel.connect()
-        
-        files = glob.glob(MUSIC_DIR+dir+"/*")
-        await context.send(files)
+@bot.command(name="CreatePlaylist", pass_context=True)
+async def CreatePlaylist(context,url, name):
+    if os.path.isdir(MUSIC_DIR+name):
+        files=glob.glob(MUSIC_DIR+name+"/*")
+        msg = "Playlist already exists, contenets are \n"
         for file in files:
-            vc.play(discord.FFmpegPCMAudio(file, executable="ffmpeg/ffmpeg"), after=lambda e: vc.stop())
+            msg = msg + file + "\n"
+        await context.send(msg)
+    else:
+        os.mkdir(MUSIC_DIR+name)
+    
+    dwn.DownloadYTPlaylist(url, MUSIC_DIR+name)
 
+    files = glob.glob(MUSIC_DIR+name+"\*")
+
+    msg = "Finished downloading playlist, contents are\n"
+    for file in files:
+        msg = msg + file + "\n"
+    
+    await context.send(msg)
+
+def play_next(context, song_queue):
+    vc = discord.utils.get(bot.voice_clients, guild=context.guild)
+    if len(song_queue) > 1:
+        del song_queue[0]
+        vc.play(discord.FFmpegPCMAudio(song_queue[0], executable=FFMPEGPath), after=lambda e: play_next(context, song_queue))
+    else:
+        vc.disconnect(context)
+
+@bot.command(name="StartPlaylist", pass_context=True)
+async def start_playlist(context, name):   
+    if not os.path.isdir(MUSIC_DIR+name):
+        await context.send("Playlist does not exist")
+        return
+
+
+    # grab the user who sent the command
+    user=context.message.author
+    voice_channel=user.voice.channel
+    # only play music if user is in a voice channel
+    if voice_channel!= None:
+        # grab user's voice channel
+        channel=voice_channel.name
+        await context.send('User is in channel: '+ channel)
+        # create StreamPlayer
+        vc= await voice_channel.connect()
+
+        files = glob.glob(MUSIC_DIR+name+"/*")
+        song_queue = list(files)
+        msg = ""
+        for file in files:
+            msg = msg + file + '\n'
+        await context.send(msg)
+        for file in files:
+            vc.play(discord.FFmpegPCMAudio(file, executable=FFMPEGPath), after=lambda e: play_next(context, song_queue))
+        # disconnect after the player has finished
+        #TODO: 
+        
+
+
+    else:
+        await context.send('User is not in a channel.')
+
+@bot.command(name="DisplayPlaylist", pass_context=True)
+async def display_playlist(context, name):
+    if not os.path.isdir(MUSIC_DIR+name):
+        context.send("Playlist does not exist")
+        return
+    files = glob.glob(MUSIC_DIR+name+"/*")
+    msg = ""
+    for index, fname in enumerate(files):
+        msg = msg + f"{index + 1} -> {fname}\n"
+
+    await context.send(msg)
+
+@bot.command(name="SelectivePlay", pass_context=True)
+async def selective_play(context, playlist_name, uid): #uid = glob.glob(<playlist-dir>)[uid - 1]
+
+    if not os.path.isdir(MUSIC_DIR+playlist_name):
+        await context.send("No playlist found!")
+        return
+    
+
+    files = glob.glob(MUSIC_DIR+playlist_name+"/*")
+    for i in range(len(files)):
+        files[i] = files[i].replace("\\", "/")
+
+    index = int(uid) - 1
+
+    if index not in range(len(files)):
+        await context.send("Invalid UID for playlist item")
+        return
+
+    # grab the user who sent the command
+    user=context.message.author
+    voice_channel=user.voice.channel
+    # only play music if user is in a voice channel
+    if voice_channel!= None:
+        # grab user's voice channel
+        channel=voice_channel.name
+        await context.send('User is in channel: '+ channel)
+        # create StreamPlayer
+        vc= await voice_channel.connect()
+        await context.send(files)
+        vc.play(discord.FFmpegPCMAudio(files[index], executable=FFMPEGPath), after=lambda e: vc.stop())
         # disconnect after the player has finished
         #TODO: 
         
     else:
         await context.send('User is not in a channel.')
 
-@bot.command(name="dc", pass_context=True)
+@bot.command(name="Disconnect", pass_context=True)
 async def disconnect(context):
     vc = discord.utils.get(bot.voice_clients, guild=context.guild)
     if vc == None:
@@ -77,57 +174,23 @@ async def disconnect(context):
         await vc.disconnect()
 
 
-@bot.command(name="addsong", pass_context=True)
+@bot.command(name="AddSong", pass_context=True)
 async def add_song(context, url, name):
-    log = dwn.DownloadVidYT(url=url, save_dir=MUSIC_DIR, filename=name)
+    log = dwn.DownloadAudioYT(url=url, save_dir=MUSIC_DIR, filename=name)
     await context.send(log)
 
-@bot.command(name="addplaylist", pass_context=True)
-async def add_playlist(context, url, name):
-    dwn.DownloadMusicPlaylistSpotify(url, MUSIC_DIR+name)
-
-@bot.command(name="fortune_cookie", pass_context=True)
-async def fortune_cookie(context):
-    sample_space = [
-        "Your mom is a hoe",
-        "Yo mama so fat she is blocking the transference signal",
-        "Yo mama so fat, when she sits in the theater, she sits next to everyone",
-        "You are nearing your end, read metamorphosis to live your life to the fullest",
-        "You are about to have an NTR experience",
-        "Einstein did not die so that you could discombobulate his theories with your stupidity",
-        "Even Noah did not have to carry this many animals",
-        "I'll frame your nuts on the wall(Wallframe deez nuts->warframe)",
-        "Imagine being a Titan main, like those people dont exist. Wait a minute, gay people exist",
-        "Habibi, stop playing",
-        "Trust me lil bro, you aint getting a girlfriend or a girl friend ever. :sob:",
-        "Habibi, its time to make your life useful and go to the towers again!",
-        "Ye hmar, you so dumb you bring disgrace to the hmar"
-    ]
-
-    await context.send(random.choice(sample_space))
-
-@bot.command(pass_contest=True)
-async def mbot_help(context):
-    await context.send(
-        f"{command_prefix}help -> to display all commands\n"+
-        f"{command_prefix}play <song-name> -> to play the song\n"+
-        f"{command_prefix}dc -> to kick the bot out of voice channel\n"+
-        f"{command_prefix}addsong <yt-url> <song-name-to-be-saved> -> adds the song to the library\n"+
-        f"{command_prefix}addplaylist <spotify-url> <playlist-name> -> adds playlist to the library\n"+
-        f"{command_prefix}playlist"+
-        f"{command_prefix}fortune_cookie -> gives out fotune cookie\n"+
-        "!!!Please be advised, the bot logs messages on the server for moderation purposes!!!\n"+
-        "launch with --noglog to disable logging"
-    )
-
 @bot.event
-async def on_command_error(ctx, error):
+async def on_command_error(context, error):
     if isinstance(error, commands.CommandNotFound):
-        await ctx.send(f"Unknown command, type {command_prefix}mbot_help for assistance")
+        await context.send(f"Unknown command, type {command_prefix}mbot_help for assistance")
+
+    print(f'Error: {error}')
 
 @bot.event
 async def on_member_join(member):
     await member.send('Please be advised, the bot logs mesages on the server for moderation purposes.')
+
+
 
 @bot.event
 async def on_message(message):
@@ -141,6 +204,7 @@ async def on_message(message):
 
 def main():
     token = input("TOKEN: ")
+
     bot.run(token)
     
 
